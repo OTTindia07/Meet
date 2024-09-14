a2a6c60a04c8a8c4b75d20016261ac535ff9ddb4
OTTindia07 <meetzankhariyaking@gmail.com>
2024-09-14 22:19:05 +0530

    Create switch.py

---
+++ custom_components/jr_panel/services/switch.py
@@ -0,0 +1,30 @@
+
+"""Switch service for JR Touch Panel."""
+from homeassistant.components.switch import SwitchEntity
+
+from .abstract_service import AbstractService
+
+class JRSwitch(SwitchEntity, AbstractService):
+    """Representation of a JR Touch Panel switch."""
+
+    @property
+    def is_on(self):
+        """Return true if the switch is on."""
+        return self.accessory.entities[self.dp_id]["value"]
+
+    async def async_turn_on(self, **kwargs):
+        """Turn the switch on."""
+        await self.set_state(True)
+
+    async def async_turn_off(self, **kwargs):
+        """Turn the switch off."""
+        await self.set_state(False)
+
+    async def update(self):
+        """Update the switch state."""
+        self.accessory.entities[self.dp_id]["value"] = await self.accessory.get_state(self.dp_id)
+
+    async def set_state(self, value):
+        """Set the switch state."""
+        await self.accessory.set_state(self.dp_id, value)
+        self.accessory.entities[self.dp_id]["value"] = value
